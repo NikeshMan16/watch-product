7e9a564923ca1286972dac4d37e26ba0ceedcf4b
NikeshMan16 <nikeshmanshrestha@gmail.com>
2024-10-25 12:17:01 +0545

    addition of data

---
+++ src/pages/product/product.js
@@ -0,0 +1,54 @@
+import product1 from '../../assets/product1.jpg';
+import product2 from '../../assets/product2.jpg';
+import product3 from '../../assets/product3.jpg';
+import product4 from '../../assets/product4.jpg';   
+import product5 from '../../assets/product5.png';
+import product6 from '../../assets/product6.jpg';
+
+export const PRODUCTS = [
+   { 
+    id: 1,
+    productName: "Longines Automatic",
+    price: 2199.99,
+    productImage : product1,
+
+    },
+   { 
+    id: 2,
+    productName: "Longines Master Collection",
+    price: 1499.99,
+    productImage : product2,
+
+    },
+   { 
+    id: 3,
+    productName: "Rolex Cellini Date Black Dial",
+    price: 8999.99,
+    productImage : product3 ,
+
+    },
+   { 
+    id: 4,
+    productName: "Rolex Cellini Moonphase",
+    price: 12909.99,
+    productImage : product4,
+
+    },
+   { 
+    id: 5,
+    productName: "Rolex Datejust",
+    price: 10099.99,
+    productImage : product5,
+
+    },
+   { 
+    id: 6,
+    productName: "Rolex Submariner Watch",
+    price: 13999.99,
+    productImage : product6,
+
+    },
+
+
+
+];
